92fa1572ed5350832738caae393173456fb6c214
matias toniolo <taekwondotae9@gmail.com>
2022-03-17 13:32:57 -0300

    now commit

---
+++ e_commerce/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+from e_commerce.models import Comic, WishList
+
+admin.site.register(Comic)
+
+class ComicAdmin(admin.ModelAdmin):
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+    list_filter= ('marvel_id','title')
+    search_fields = ['title']
+    
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
+
+
+admin.site.register(WishList)
+
+class wish_listAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'comic_id', 'favorite', 'cart')
+    list_display_links = ('user_id', 'comic_id')
+    list_filter= ('favorite','cart')

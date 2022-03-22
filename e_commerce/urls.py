110fc9284a7d15eef3b745576d5f4e9e7a9c7856
matias toniolo <taekwondotae9@gmail.com>
2022-03-22 15:36:43 -0300

    cambios

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from . views import BaseView, IndexView
+from . views import IndexView
 
 
 urlpatterns = [
-    path('base-view', BaseView.as_view(), name='base-view'),
     path('index', IndexView.as_view(), name='index'),
 ]
--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,16 +1,17 @@
 from django.views.generic import TemplateView
 from . models import Comic
 
-
-class BaseView(TemplateView):
-    template_name = 'e-commerce/base.html'
     
 class IndexView(TemplateView):
     template_name = 'e-commerce/index.html'
-    
+    #retorna tabla con comics disponibles
     def get_context_data(self,*args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['comic'] = Comic.objects.all()
         return context
+   
+  
+        
+        
     
     

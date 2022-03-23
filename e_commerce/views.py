8a0fdeb934725ab037788699e2d99f863c7b3ad9
matias toniolo <taekwondotae9@gmail.com>
2022-03-23 20:55:17 -0300

    Parcial

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,7 +1,9 @@
 from django.views.generic import TemplateView
 from . models import Comic
 
-    
+
+'''Todo es parcial'''
+  
 class IndexView(TemplateView):
     template_name = 'e-commerce/index.html'
     #retorna tabla con comics disponibles

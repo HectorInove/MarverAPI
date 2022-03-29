f6288a546061c1cded8dd7d778f3e5a706895d0d
matias toniolo <taekwondotae9@gmail.com>
2022-03-29 20:11:34 -0300

    cambios de proteccion

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from . views import *
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('register', register, name='register'),
     path('login', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('purchased', PurchaseView.as_view(), name='purchased'),
-    path('table', TableView.as_view(), name='table'),
-    path('cart', CartView.as_view(), name='cart'),
-    path('favorites', FavoriteView.as_view(), name='favorites'),
-    path('user-data', UserDataView.as_view(), name='user-data'),
+    path('purchased', login_required(PurchaseView.as_view()), name='purchased'),
+    path('table', login_required(TableView.as_view()), name='table'),
+    path('cart', login_required(CartView.as_view()), name='cart'),
+    path('favorites', login_required(FavoriteView.as_view()), name='favorites'),
+    path('user-data', login_required(UserDataView.as_view()), name='user-data'),
     path('saludo', SaludoView.as_view(), name='saludo'),
     path('contacto', ContacView.as_view(), name='contacto'),
     path('mail', RecivedView.as_view(), name='mail'),
